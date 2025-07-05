98579fd23747c01619b5a40a623a5c793f50db24
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-07-05 23:39:43 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS Order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
